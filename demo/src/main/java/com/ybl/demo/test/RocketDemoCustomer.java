04d652d5c7e28eda37027b68429fae10e08fbfed
user <333@qqsk.com>
2019-12-01 23:36:40 +0800

    rocket

--- demo/src/main/java/com/ybl/demo/test/RocketDemoCustomer.java
+++ demo/src/main/java/com/ybl/demo/test/RocketDemoCustomer.java
@@ -1,5 +1,13 @@
 package com.ybl.demo.test;
 
+import java.util.List;
+import org.apache.rocketmq.client.consumer.DefaultMQPushConsumer;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyContext;
+import org.apache.rocketmq.client.consumer.listener.ConsumeConcurrentlyStatus;
+import org.apache.rocketmq.client.consumer.listener.MessageListenerConcurrently;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageExt;
+
 /**
  * @ProjectName: demo
  * @Package: com.ybl.demo.test
@@ -11,7 +19,56 @@ package com.ybl.demo.test;
  */
 public class RocketDemoCustomer {
 
-    public void customer(){
+//
+//    public static void main(String[] args) {
+//        try {
+//            customer();
+//        } catch (MQClientException e) {
+//            e.printStackTrace();
+//        }
+//    }
+//
+//    public static void customer() throws MQClientException {
+//        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("test_rocket_group");
+//        consumer.setNamesrvAddr("192.168.0.199:9876");
+//        consumer.subscribe("test_topic_aabb","*");
+//        consumer.registerMessageListener(new MessageListenerConcurrently() {
+//            @Override
+//            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+//                ConsumeConcurrentlyContext consumeConcurrentlyContext) {
+//                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+//                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+//            }
+//        });
+//        consumer.start();
+//        System.out.printf("Consumer Started.%n");
+//    }
 
+    public static void main(String[] args) throws InterruptedException, MQClientException {
+
+        // Instantiate with specified consumer group name.
+        DefaultMQPushConsumer consumer = new DefaultMQPushConsumer("please_rename_unique_group_name");
+
+        // Specify name server addresses.
+        consumer.setNamesrvAddr("192.168.0.199:9876");
+
+        // Subscribe one more more topics to consume.
+        consumer.subscribe("TopicTest", "*");
+        // Register callback to execute on arrival of messages fetched from brokers.
+        consumer.registerMessageListener(new MessageListenerConcurrently() {
+
+            @Override
+            public ConsumeConcurrentlyStatus consumeMessage(List<MessageExt> msgs,
+                ConsumeConcurrentlyContext context) {
+                System.out.printf("%s Receive New Messages: %s %n", Thread.currentThread().getName(), msgs);
+                return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+            }
+        });
+
+        //Launch the consumer instance.
+        consumer.start();
+
+        System.out.printf("Consumer Started.%n");
     }
+
 }
--- demo/src/main/java/com/ybl/demo/test/RocketDemoProducer.java
+++ demo/src/main/java/com/ybl/demo/test/RocketDemoProducer.java
@@ -4,6 +4,7 @@ import org.apache.rocketmq.client.exception.MQClientException;
 import org.apache.rocketmq.client.producer.DefaultMQProducer;
 import org.apache.rocketmq.client.producer.SendResult;
 import org.apache.rocketmq.common.message.Message;
+import org.apache.rocketmq.remoting.common.RemotingHelper;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -18,27 +19,55 @@ import org.springframework.beans.factory.annotation.Value;
 public class RocketDemoProducer {
 
     @Value("rocket.server.host")
-    public static  String rocketMqServerIp;
+    public static String rocketMqServerIp;
 
     public static void main(String[] args) {
-
+        try {
+//            syncProducer();
+            example();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     /**
      * 同步消息
      */
-    public static void SyncProducer() throws Exception {
+    public static void syncProducer() throws Exception {
         //创建一个mqGroup
         DefaultMQProducer producer = new DefaultMQProducer("test_rocket_group");
         //配置mq地址
-        producer.setNamesrvAddr(rocketMqServerIp);
+        producer.setNamesrvAddr("192.168.0.199:9876");
         producer.start();
-        Message message = new Message("test_topic", "tags_test", "hello rocket".getBytes());
+        Message message = new Message("test_topic", "tags_test", "hello rocket".getBytes(RemotingHelper.DEFAULT_CHARSET));
         //发送消息
         SendResult sendResult = producer.send(message);
         //输出打印发送结果
         System.out.printf("%s%n", sendResult);
         //关掉生产者
+        producer.shutdown();
+    }
+
+    public static void example ()throws Exception{
+        //Instantiate with a producer group name.
+        DefaultMQProducer producer = new
+            DefaultMQProducer("please_rename_unique_group_name");
+        // Specify name server addresses.
+        producer.setNamesrvAddr("192.168.0.199:9876");
+        //Launch the instance.
+        producer.start();
+        for (int i = 0; i < 100; i++) {
+            //Create a message instance, specifying topic, tag and message body.
+            Message msg = new Message("TopicTest" /* Topic */,
+                "TagA" /* Tag */,
+                ("Hello RocketMQ " +
+                    i).getBytes(RemotingHelper.DEFAULT_CHARSET) /* Message body */
+            );
+            //Call send message to deliver message to one of brokers.
+            SendResult sendResult = producer.send(msg);
+            System.out.printf("%s%n", sendResult);
+        }
+        //Shut down once the producer instance is not longer in use.
         producer.shutdown();
     }
 
